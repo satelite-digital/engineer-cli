19b96408308dbfd9d99b7240f566dd8855502b2f
Erick Ruano <erickaleruano@gmail.com>
2020-09-09 22:05:23 -0600

    Added options to init

--- cmds/init/index.js
+++ cmds/init/index.js
@@ -1,14 +1,62 @@
+const inquirer = require("inquirer")
+const fs = require("fs")
 const createDirectoryContents = require("./createDirectoryContents")
+
+const CURR_DIR = process.cwd();
+const CHOICES = [ "Current project", ...fs.readdirSync(`${__dirname}/templates`) ]; 
+
+
+
+const PROJECT_QUESTION = {
+    name: 'project-choice',
+    type: 'list',
+    message: 'What project template would you like to generate?',
+    choices: CHOICES
+  }
+
+const NAME_QUESTION = {
+    name: 'project-name',
+    type: 'input',
+    message: 'Project name:',
+    validate: function (input) {
+      if (/^([A-Za-z\-\_\d])+$/.test(input)) return true;
+      else return 'Project name may only include letters, numbers, underscores and hashes.';
+    }
+  }
+
+
 
 module.exports = (args) => {
 
-    const projectChoice = 'init';
-    const projectName = false;
-    const templatePath = `${__dirname}/templates/${projectChoice}`;
+  inquirer.prompt([PROJECT_QUESTION])
+  .then(answers => {
+    
+    
+    let projectChoice;
+    let projectName = false;
+    if(answers['project-choice'] == 'Current project'){
+      projectChoice = 'init';
+      projectName = false;
+      const templatePath = `${__dirname}/templates/${projectChoice}`;
+      if(projectName){
+        fs.mkdirSync(`${CURR_DIR}/${projectName}`);
+      }
   
-    if(projectName){
-      fs.mkdirSync(`${CURR_DIR}/${projectName}`);
+      createDirectoryContents(templatePath, projectName);
+    }else{
+      projectChoice = answers['project-choice']
+      inquirer.prompt([NAME_QUESTION]).then(answers => {
+        projectName = answers['project-name']
+        const templatePath = `${__dirname}/templates/${projectChoice}`;
+        if(projectName){
+          fs.mkdirSync(`${CURR_DIR}/${projectName}`);
+        }
+    
+        createDirectoryContents(templatePath, projectName);
+      })
     }
+  
+    
 
-    createDirectoryContents(templatePath, projectName);
+  });
 }
---
+++ cmds/init/templates/node-rest/engineer.config.js
@@ -0,0 +1,13 @@
+const config = {
+  "options" : {
+    "model" : "./.engineer/data.json"
+  },
+  "resources" : [
+    {
+      "src" : "./.engineer/files/code.js",
+      "dest" : "./src/code.js"
+    }
+  ]
+}
+
+module.exports = config
